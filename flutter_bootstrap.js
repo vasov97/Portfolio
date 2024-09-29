76655636a90b226f4fc3692b998f30ee85bb5cf2
vasov97 <vasovmail@gmail.com>
2024-09-29 10:45:05 +0200

    new ff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2256707392"
+    serviceWorkerVersion: "1310698795"
   }
 });
